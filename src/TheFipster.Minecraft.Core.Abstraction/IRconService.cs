761516854deb391fea2ec1b28de60ab98cb101f7
Felix Reisch <felix.reisch.85@gmail.com>
2020-12-14 18:44:48 +0100

    Tested general rcon service and added seed query using the rcon service,

--- src/TheFipster.Minecraft.Core.Abstraction/IRconService.cs
+++ src/TheFipster.Minecraft.Core.Abstraction/IRconService.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
 
 namespace TheFipster.Minecraft.Core.Abstractions
 {
     public interface IRconService
     {
+        Task<string> SendAsync(string command);
     }
 }
