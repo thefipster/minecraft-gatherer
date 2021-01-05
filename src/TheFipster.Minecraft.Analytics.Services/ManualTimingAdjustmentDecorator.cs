1ae43099f34b368a15faa551395a100fbda47d1c
Felix Reisch <felix.reisch.85@gmail.com>
2021-01-05 06:07:51 +0100

    Started new decorator to adjust timing on manual timing overwrite.

---
+++ src/TheFipster.Minecraft.Analytics.Services/ManualTimingAdjustmentDecorator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using TheFipster.Minecraft.Analytics.Abstractions;
+using TheFipster.Minecraft.Core.Domain;
+using TheFipster.Minecraft.Manual.Abstractions;
+using TheFipster.Minecraft.Manual.Domain;
+
+namespace TheFipster.Minecraft.Analytics.Services
+{
+    public class ManualTimingAdjustmentDecorator : IManualsWriter
+    {
+        private readonly IManualsWriter _component;
+        private readonly IAnalyticsReader _reader;
+
+        public ManualTimingAdjustmentDecorator(IManualsWriter writer, IAnalyticsReader reader)
+        {
+            _component = writer;
+            _reader = reader;
+        }
+
+        public bool Exists(string name)
+            => _component.Exists(name);
+
+        public void Upsert(RunManuals manuals)
+        {
+            _component.Upsert(manuals);
+
+            if (!manuals.RuntimeInMs.HasValue)
+                return;
+
+            var analytics = _reader.Get(manuals.Worldname);
+
+            if (!analytics.Timings.FinishedOn.HasValue)
+                return;
+
+            if (!analytics.Timings.Events.Any(x => x.Section == Sections.TheEnd))
+                return;
+
+            analytics.Timings.RunTime = TimeSpan.FromMilliseconds(manuals.RuntimeInMs.Value);
+
+            var timeWithoutEnd = analytics.Timings.Events.Where(x => x.Section != Sections.TheEnd).Sum(x => x.Time.Milliseconds);
+            var timeInEnd = manuals.RuntimeInMs.Value - timeWithoutEnd;
+
+            if (timeInEnd <= 0)
+                return;
+
+            var newTimeInEnd = TimeSpan.FromMilliseconds(timeInEnd);
+
+            throw new NotImplementedException();
+        }
+    }
+}
